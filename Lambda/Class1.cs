762e4457a987cb66825ab3eea0fa61ac3bccecb5
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-28 14:04:24 -0600

    pitoooo

--- Lambda/Class1.cs
+++ Lambda/Class1.cs
@@ -9,12 +9,51 @@ namespace Lambda
 {
     public class Class1
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
-            Action<string> Saludo1 = cadena =>
-            {
+            //Action<string> Saludo1 = cadena =>
+            //{
+            //    Console.WriteLine($"Hola {cadena} ");
+            //};
+            //Saludo1("Pedro");
 
-            }; 
+            //Action<string, string> Saludo2 = (cadena1, cadena2) =>
+            //{
+            //    Console.WriteLine($"Hola {cadena1}, {cadena2} ");
+            //};
+            //Saludo2("Juan", "Martinez");
+
+
+            List<int> ListadoNumero = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            ListadoNumero.ForEach(numerito =>
+            {
+                if (numerito % 2 == 0)
+                {
+                    Console.WriteLine(numerito);
+                }
+            });
+
+
+            List<Persona0> ListadoSinFiltro = new List<Persona0>
+            {
+                new Persona0() {Nombre = "Juan", Apellido = "Martinez", Edad= 18},
+                new Persona0() {Nombre = "Oscar", Apellido = "Garcia", Edad= 18},
+                new Persona0() {Nombre = "Cesar", Apellido = "Vivas", Edad= 18}
+            };
+
+            //foreach (var item in ListadoSinFiltro.Where(Object=> Object.Edad>18))
+            //{
+            //    Console.WriteLine(item.Nombre);
+            //}
+
+            var ListadoConFiltro = ListadoSinFiltro.Where(x => x.Apellido.Contains("M"));
+
+            foreach(var item in ListadoConFiltro)
+            {
+                Console.WriteLine(item.Nombre);
+            }
+
+            Console.ReadLine();
         }
 
     }
